97ea07cf1201bc26638ee055e7bc8f47daf0d3fc
u225782 <Olivier.Ory@ulg.ac.be>
2016-12-21 15:11:25 +0100

    Dependency: github.com/normegil/log

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 	"strconv"
 
 	"github.com/normegil/aphrodite/router"
+	"github.com/Normegil/log"
 )
 
 const PORT int = 8080
 
 func main() {
-	err := http.ListenAndServe(":"+strconv.Itoa(PORT), router.New())
+	log := log.StructuredLog{
+
+	}
+	itoa := strconv.Itoa(PORT)
+	log.Log(log.DEBUG, log.Structure{}, "Listening on " + itoa)
+	err := http.ListenAndServe(":"+ itoa, router.New())
 	if nil != err {
 		fmt.Print(err)
 	}
