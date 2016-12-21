c604f1b909504ff0fd8616db5a44ddf6a3dd38cd
u225782 <Olivier.Ory@ulg.ac.be>
2016-12-21 16:11:43 +0100

    Add some logging

--- main.go
+++ main.go
@@ -6,12 +6,17 @@ import (
 	"strconv"
 
 	"github.com/normegil/aphrodite/router"
+	"github.com/sirupsen/logrus"
 )
 
 const PORT int = 8080
 
 func main() {
-	err := http.ListenAndServe(":"+ strconv.Itoa(PORT), router.New())
+	log := logrus.New()
+	log.Formatter = &logrus.TextFormatter{}
+	port := strconv.Itoa(PORT)
+	log.WithField("port", PORT).Info("Server listening")
+	err := http.ListenAndServe(":"+ port, router.New())
 	if nil != err {
 		fmt.Print(err)
 	}
